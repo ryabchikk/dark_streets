bd6de09515da81b3e3019d265f9184fb13b6c286
selfmadekirich <tosha.kirichenko.2001@mail.ru>
2023-11-04 18:31:26 +0300

    basic synch.first step

--- Assets/Scripts/Networking_test/PlayerNetwork.cs
+++ Assets/Scripts/Networking_test/PlayerNetwork.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerNetwork : MonoBehaviour
+public class PlayerNetwork : NetworkBehaviour
 {
   private  void Update()
   {
+    if(!IsOwner) 
+        return;
     Vector3 move = new Vector3(0,0,0);
     if(Input.GetKey(KeyCode.W)) move.z = 1f;
     if(Input.GetKey(KeyCode.S)) move.z = -1f;
